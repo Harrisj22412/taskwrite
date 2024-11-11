b715a78db1d6dce27df5e5d01dd55c2fe8272e32
Jalen Harris <Harrisj22412@hocking.edu>
2024-11-11 13:22:14 -0500

    Connected to Appwrite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        error: "var(--text-error)",
+        ok: "var(--text-ok)",
+        main: "var(--text-main)",
+        iconColor: "var(--btn-icon-main)",
+    },
+    backgroundColor: {
+        base: "var(--base-bg)",
+        primary: "var(--btn-bg-primary)",
+        primaryHover: "var(--btn-bg-primary-hover)",
+        ok: "var(--btn-bg-ok)",
+        lightOk: "var(--btn-bg-light-ok)",
+        light: "var(--btn-bg-light)",
+        lowPriority: "var(--low-priority)",
+        mediumPriority: "var(--medium-priority)",
+        highPriority: "var(--high-priority)",
+    },
+    borderColor: {
+        container: "var(--border-container)",
+        input: "var(--border-input)",
+        error: "var(--border-error)",
+    },
+    },
   },
   plugins: [],
 }
